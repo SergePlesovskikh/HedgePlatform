ac7bf08e51a7b52f41b0c231e512e3acacbfb544
Сергей Плесовских <sergredples@rambler.ru>
2020-04-30 15:36:46 +1000

    Counter API in progress

--- HedgePlatform/Controllers/API/Counter/CounterController.cs
+++ HedgePlatform/Controllers/API/Counter/CounterController.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
+using HedgePlatform.BLL.Interfaces;
+using HedgePlatform.BLL.Infr;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using AutoMapper;
+using HedgePlatform.ViewModel.API;
+using HedgePlatform.BLL.DTO;
 
 namespace HedgePlatform.Controllers.API
 {
@@ -11,5 +13,43 @@ namespace HedgePlatform.Controllers.API
     [ApiController]
     public class CounterController : ControllerBase
     {
+        ICounterService counterService;
+        public CounterController(ICounterService service)
+        {
+            counterService = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<CounterViewModel> Index()
+        {
+            IEnumerable<CounterDTO> counterDTOs = counterService.GetCounters();
+
+            var mapper = new MapperConfiguration(cfg => {
+                cfg.CreateMap<CounterDTO, CounterViewModel>()
+                .ForMember(s => s.CounterType, h => h.MapFrom(src => src.CounterType));       
+               
+             //   cfg.CreateMap<CounterTypeDTO, CounterTypeViewModel>();
+             //   cfg.CreateMap<CounterStatusDTO, CounterStatusViewModel>();
+            }).CreateMapper();
+
+            var counters = mapper.Map<IEnumerable<CounterDTO>, List<CounterViewModel>>(counterDTOs);
+            return counters;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CounterViewModel counter)
+        {
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<CounterViewModel, CounterDTO>()).CreateMapper();
+            var counterDTO = mapper.Map<CounterViewModel, CounterDTO>(counter);
+            try
+            {
+                counterService.CreateCounter(counterDTO);
+                return Ok("Ok");
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
---
+++ HedgePlatform/ViewModel/API/CounterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HedgePlatform.ViewModel.API
+{
+    public class CounterViewModel
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public string Location { get; set; }
+        public int CounterTypeId { get; set; }
+        public CounterTypeViewModel CounterType {get;set;}
+        public CounterValueViewModel LastCounterValue { get; set; }
+        public bool? AddValues { get; set; }
+    }
+}
